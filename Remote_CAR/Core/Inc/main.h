026811927a9ce2b2f15cdf481f708221b549c497
xlxlqqq <xlxlqqq@163.com>
2021-09-15 23:30:04 +0800

    Add Remote Car Project.

---
+++ Remote_CAR/Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RF_Pos_Pin GPIO_PIN_0
+#define RF_Pos_GPIO_Port GPIOA
+#define RF_Nag_Pin GPIO_PIN_1
+#define RF_Nag_GPIO_Port GPIOA
+#define LB_Pos_Pin GPIO_PIN_6
+#define LB_Pos_GPIO_Port GPIOA
+#define LB_Nag_Pin GPIO_PIN_7
+#define LB_Nag_GPIO_Port GPIOA
+#define LF_Pos_Pin GPIO_PIN_4
+#define LF_Pos_GPIO_Port GPIOC
+#define LF_Nag_Pin GPIO_PIN_5
+#define LF_Nag_GPIO_Port GPIOC
+#define RB_Pos_Pin GPIO_PIN_14
+#define RB_Pos_GPIO_Port GPIOB
+#define RB_Nag_Pin GPIO_PIN_15
+#define RB_Nag_GPIO_Port GPIOB
+#define LED_Pin GPIO_PIN_2
+#define LED_GPIO_Port GPIOD
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
